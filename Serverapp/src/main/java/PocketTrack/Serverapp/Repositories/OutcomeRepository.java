70442f41495f59985a41b22c2d23723d2d9dc85e
fajar <fajar.anwarimaulana99@gmail.com>
2024-02-26 15:44:38 +0700

    feat(outcome): insert outcome and getAll outcome with pagination

--- Serverapp/src/main/java/PocketTrack/Serverapp/Repositories/OutcomeRepository.java
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Repositories/OutcomeRepository.java
@@ -1,7 +1,11 @@
 package PocketTrack.Serverapp.Repositories;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import PocketTrack.Serverapp.Domains.Entities.Outcome;
@@ -10,4 +14,13 @@ import PocketTrack.Serverapp.Repositories.Base.BaseRepositories;
 @Repository
 public interface OutcomeRepository extends BaseRepositories<Outcome, String> {
     Optional<Outcome> findByTitle(String title);
+
+    Optional<Outcome> findByDate(LocalDateTime date);
+
+    @Query("SELECT o FROM Outcome o WHERE o.amount = :amount")
+    Optional<List<Outcome>> getAllByAmount(BigDecimal amount);
+
+    Optional<List<Outcome>> findByStatusFalse();
+
+    Optional<List<Outcome>> findByStatusTrue();
 }
--- Serverapp/src/main/java/PocketTrack/Serverapp/Services/Interfaces/OutcomeService.java
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Services/Interfaces/OutcomeService.java
@@ -1,13 +1,40 @@
 package PocketTrack.Serverapp.Services.Interfaces;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import PocketTrack.Serverapp.Domains.Entities.Outcome;
+import PocketTrack.Serverapp.Domains.Models.Requests.OutcomeRequest;
+import PocketTrack.Serverapp.Domains.Models.Responses.ObjectResponseData;
 import PocketTrack.Serverapp.Domains.Models.Responses.ResponseData;
 import PocketTrack.Serverapp.Services.Interfaces.Base.BaseServices;
 
 public interface OutcomeService extends BaseServices<Outcome, String> {
     Outcome getByTitle(String title);
 
+    Outcome getByDate(LocalDateTime date);
+
+    List<Outcome> getByAmount(BigDecimal amount);
+
+    List<Outcome> getFalseStatus(Boolean status);
+
+    List<Outcome> getTrueStatus(Boolean status);
+
     ResponseEntity<ResponseData<Outcome>> getByTitleWithResponse(String title);
+
+    ResponseEntity<ResponseData<Outcome>> getByDateWithResponse(LocalDateTime date);
+
+    ResponseEntity<ResponseData<List<Outcome>>> getByAmountWithResponse(BigDecimal amount);
+
+    ResponseEntity<ResponseData<List<Outcome>>> getByStatusFalseWithResponse(Boolean status);
+
+    ResponseEntity<ResponseData<List<Outcome>>> getByStatusTrueWithResponse(Boolean status);
+
+    ObjectResponseData<Outcome> getAllOutcome(LocalDateTime date, String title, BigDecimal amount, int page, int size);
+
+    ResponseEntity<ResponseData<Outcome>> insertOutcome(OutcomeRequest outcomeRequest);
+
 }

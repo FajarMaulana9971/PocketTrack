0eb5bcc11d3894ab69d0a39a87f4849725e9a0ea
fajar <fajar.anwarimaulana99@gmail.com>
2024-03-27 13:02:37 +0700

    feat(Auth): auth controller unfinished

---
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Controllers/AuthController.java
@@ -0,0 +1,48 @@
+package PocketTrack.Serverapp.Controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import PocketTrack.Serverapp.Domains.Models.ErrorData;
+import PocketTrack.Serverapp.Domains.Models.LoginData;
+import PocketTrack.Serverapp.Domains.Models.RegisterData;
+import PocketTrack.Serverapp.Domains.Models.Responses.LoginResponse;
+import PocketTrack.Serverapp.Services.Interfaces.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    // @Operation(summary = "This endpoint is used to register a new user")
+    @PostMapping("register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterData registerData, Errors errors) {
+        if (errors.hasErrors()) {
+            List<ErrorData> errorDataList = new ArrayList<>();
+            errors.getAllErrors().forEach(error -> errorDataList.add(new ErrorData(error.getDefaultMessage())));
+            return ResponseEntity.badRequest().body(errorDataList);
+        }
+        return authService.register(registerData);
+    }
+
+    @PostMapping("login")
+    public LoginResponse login(@RequestBody LoginData loginData, HttpServletResponse response) {
+        return authService.login(loginData, response);
+    }
+
+    @PostMapping("refresh-token")
+    public LoginResponse refreshToken(@CookieValue(value = "refreshToken") String accessToken) {
+        return authService.refreshToken(accessToken);
+    }
+
+}

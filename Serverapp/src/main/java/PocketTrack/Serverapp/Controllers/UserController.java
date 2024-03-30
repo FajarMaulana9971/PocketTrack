083cfd08f1b444fa38b1c366f8d7d7c3b586ba51
fajar <fajar.anwarimaulana99@gmail.com>
2024-03-30 11:32:02 +0700

    feat(user): add method get users on controller

--- Serverapp/src/main/java/PocketTrack/Serverapp/Controllers/UserController.java
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Controllers/UserController.java
@@ -3,9 +3,11 @@ package PocketTrack.Serverapp.Controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import PocketTrack.Serverapp.Domains.Models.Responses.ObjectResponseData;
+import PocketTrack.Serverapp.Domains.Models.Responses.UserResponse;
 import PocketTrack.Serverapp.Services.Interfaces.UserService;
 import lombok.AllArgsConstructor;
 
@@ -37,6 +39,14 @@ public class UserController {
     @GetMapping("admin/users/{id}")
     public ResponseEntity<?> getUserById(@PathVariable String id) {
         return userService.getByIdWithResponse(id);
+    }
+
+    @GetMapping("admin/users")
+    public ObjectResponseData<UserResponse> getUsers(
+            @RequestParam(required = false) String keywoard,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size) {
+        return userService.getAllUserWithPagination(keywoard, page, size);
     }
 
 }

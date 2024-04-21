218784ecebb7fc41ddea32e480ccb702e0c3dde9
fajar <fajar.anwarimaulana99@gmail.com>
2024-04-22 05:49:21 +0700

    fix(histories): add controller

---
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Controllers/HistoryController.java
@@ -0,0 +1,13 @@
+package PocketTrack.Serverapp.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping
+public class HistoryController {
+
+}

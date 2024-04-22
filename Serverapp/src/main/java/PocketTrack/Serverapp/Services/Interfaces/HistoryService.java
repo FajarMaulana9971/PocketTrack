c371f5d9de310537d9901d56d91a317a3befcabb
fajar <fajar.anwarimaulana99@gmail.com>
2024-04-22 11:48:30 +0700

    feat(history&budget): add method get all history and fix typo comment

--- Serverapp/src/main/java/PocketTrack/Serverapp/Services/Interfaces/HistoryService.java
+++ Serverapp/src/main/java/PocketTrack/Serverapp/Services/Interfaces/HistoryService.java
@@ -1,8 +1,11 @@
 package PocketTrack.Serverapp.Services.Interfaces;
 
 import PocketTrack.Serverapp.Domains.Entities.History;
+import PocketTrack.Serverapp.Domains.Models.Responses.ObjectResponseData;
 import PocketTrack.Serverapp.Services.Interfaces.Base.BaseServices;
 
 public interface HistoryService extends BaseServices<History, String> {
+
+    ObjectResponseData<History> getAllHistories(String historyId, String keywoard, int page, int size);
 
 }
